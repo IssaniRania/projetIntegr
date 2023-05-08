5a4a2de8ad8f2d377d7c95bd2d2a3ab76bc21158
IssaniRania <raniaissani83@gmail.com>
2023-05-08 20:43:53 +0100

    entitie

---
+++ src/main/java/com/example/projetintegr/dao/CertifRepository.java
@@ -0,0 +1,7 @@
+package com.example.projetintegr.dao;
+
+import com.example.projetintegr.entities.Certif;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CertifRepository extends JpaRepository<Certif, Long> {
+}
